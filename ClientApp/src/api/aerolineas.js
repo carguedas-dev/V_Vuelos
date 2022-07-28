47a5ab86fb828e38a26046c141896f2e4d1e3713
brandonrq506 <darkrahd@gmail.com>
2022-07-28 09:56:54 -0600

    Completado Aerolineas, hice getPais y mejoré Puerta

--- ClientApp/src/api/aerolineas.js
+++ ClientApp/src/api/aerolineas.js
@@ -1,25 +1,47 @@
 import axios from "axios";
+import { getPaises, getPais } from "./pais";
 
 const baseURL = 'http://localhost:58214/api/Aerolineas/';
 
 export const getAerolineas = async () => {
     let aerolineasResponse = await axios.get(baseURL);
     let aerolineas = aerolineasResponse.data;
-    console.log(aerolineas)
-    //Importar Pais....
+    let paises = await getPaises();
+    for (const aerolinea of aerolineas) {
+        for (const pais of paises) {
+            if (aerolinea.pais === pais.id) {
+                aerolinea.pais = pais.nombre;
+                break;
+            }
+        }
+    }
     return aerolineas;
 }
 
 export const getAerolinea = async id => {
     let response = await axios.get(`${baseURL}${id}`);
     let aerolinea = response.data;
-
+    let pais = await getPais(aerolinea.pais);
+    aerolinea.pais = pais;
+    return aerolinea;
 }
 
-export const putAerolinea = async (id) => {
+export const postAerolinea = async (nombre, imagen, pais) => {
+    axios.post(baseURL, {
+        nombre: nombre,
+        imagen: imagen,
+        pais: pais
+    });
+}
 
+export const putAerolinea = async (id, nombre, imagen, pais) => {
+    axios.put(`${baseURL}${id}`, {
+        nombre: nombre,
+        Imagen: imagen,
+        pais: pais
+    });
 }
 
 export const deleteAerolinea = async id => {
-
+    axios.delete(`${baseURL}${id}`);
 }
--- ClientApp/src/api/pais.js
+++ ClientApp/src/api/pais.js
@@ -5,7 +5,6 @@ const baseURL = 'http://localhost:58214/api/Paises/';
 export const getPaises = async () => {
     let response = await axios.get(baseURL);
     let paises = response.data;
-    console.log(paises)
     return paises;
 }
 
@@ -20,5 +19,5 @@ export const putPais = async (id) => {
 }
 
 export const deletePais = async id => {
-
+    axios.delete(`${baseURL}${id}`);
 }
