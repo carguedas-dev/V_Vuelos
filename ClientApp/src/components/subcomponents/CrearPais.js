170737e2ac211dc22b0aeb78fb7f7e31b3f4e176
brandonrq506 <darkrahd@gmail.com>
2022-07-29 10:43:12 -0600

    Paises FULL CRUD and auto-Update

--- ClientApp/src/components/subcomponents/CrearPais.js
+++ ClientApp/src/components/subcomponents/CrearPais.js
@@ -1,20 +1,30 @@
+import { useRef } from "react";
+const CrearPais = props => {
 
-const CrearPais = () => {
+    const nombre = useRef()
+    const image = useRef()
+
+    const onAddCountry = e => {
+        e.preventDefault();
+        props.onAddCountry(nombre.current.value, image.current.value);
+        e.target.reset();
+    }
+
     return (
         <div className='d-flex flex-column'>
             <div className='row'>
                 <h3>Add Country</h3>
             </div>
 
-            <form action="">
+            <form action="" onSubmit={onAddCountry}>
                 <div className="row">
                     <div className="col-md-3">
                         <label for="country" className="form-label">Country</label>
-                        <input type="text" className="form-control" id="country" />
+                        <input type="text" className="form-control" id="country" ref={nombre} />
                     </div>
                     <div className="col-4 mb-3">
                         <label for="formFile" className="form-label">Flag</label>
-                        <input className="form-control" type="file" id="formFile" />
+                        <input className="form-control" type="file" id="formFile" ref={image} />
                     </div>
                 </div>
 
